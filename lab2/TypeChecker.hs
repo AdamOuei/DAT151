ca2e493efeedcfa4c8e7e509738a24b552154923
Omar Oueidat <oueidat@student.chalmers.se>
2019-11-21 13:51:32 +0100

    Make prettier code

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -18,10 +18,14 @@ type Sig = Map Id ([Type],Type) -- function type signature
 type Context = Map Id Type -- variables with their types
 
 lookupVar :: Env -> Id -> Err Type
+lookupVar (_, []) id = Bad "Variable does not exist"
+lookupVar (sig, (top:stack)) id | lookup id top == Nothing = lookupVar (sig, stack) id 
+                                | otherwise = Ok $ fromJust  $ lookup id top
+
 lookupVar (_, stack) id = 
           case catMaybes maybeList of
             [] -> Bad "No variable found"
-            [Just x:_] -> Ok x
+            [Just boomer:_] -> Ok boomer
             where maybeList = map lookup id stack
             
 
@@ -32,7 +36,7 @@ lookupFun (sig, _) id =
                 Just pair -> Ok pair
 
 updateVar :: Env -> Id -> Type -> Err Env
-updateVar (sig, stack) 
+updateVar (sig, stack) typ =
 
 updateFun :: Env -> Id -> ([Type],Type) -> Err Env
 newBlock :: Env -> Env
