29feaf93a96b8aac2529d0f6b2eb7684c2c1ec77
tuubaas <tobli@student.chalmers.se>
2019-11-25 13:30:18 +0100

    Finished exp and started on stms

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -19,80 +19,85 @@ type Sig = Map Id ([Type],Type) -- function type signature
 type Context = Map Id Type -- variables with their types
 
 inferExp :: Env -> Exp -> Err Type
-inferExp env exp =
-            case exp of
-                EInt Integer -> Ok Integer
-                EDouble Double -> Ok Double
-                ETrue -> Ok Bool
-                EFalse -> Ok Bool
-                EId id -> lookupVar env id
-                ECall id argExps -> do
-                            case lookupFun env id of
-                                Bad s -> Bad s
-                                Ok (argsTypes, typ) -> do
-                                                let argsCheck = zipWith (\x y -> checkExp env x y) argTypes argExps
-                                                case any isBad argsCheck of
-                                                    True -> Bad "Arguments not ok"
-                                                    False -> Ok typ
+inferExp env exp = case exp of
+                    EInt Integer -> Ok Integer
+                    EDouble Double -> Ok Double
+                    ETrue -> Ok Bool
+                    EFalse -> Ok Bool
+                    EId id -> lookupVar env id
+                    ECall id argExps -> do
+                                    case lookupFun env id of
+                                        Bad s -> Bad s
+                                        Ok (argsTypes, typ) -> do
+                                                            let argsCheck = zipWith (\x y -> checkExp env x y) argTypes argExps
+                                                            case any isBad argsCheck of
+                                                                True -> Bad "Arguments not ok"
+                                                                False -> Ok typ
                 -- Maybe change to TBool and add void
-                EInc id -> case lookupVar env id of
-                            Ok Bool -> Bad "Not valid type"
-                            _ -> lookupVar env id
-                EDec id ->  case lookupVar env id of
-                            Ok Bool -> Bad "Not valid type"
-                            _ -> lookupVar env id
-                EInc2 id ->  case lookupVar env id of
-                            Ok Bool -> Bad "Not valid type"
-                            _ -> lookupVar env id
-                EDec2 id ->  case lookupVar env id of
-                            Ok Bool -> Bad "Not valid type"
-                            _ -> lookupVar env id
-                EMul exp1 exp2 -> inferBin env exp1 exp2
-                EDiv exp1 exp2 -> inferBin env exp1 exp2
-                EAdd exp1 exp2 -> inferBin env exp1 exp2
-                ESub exp1 exp2 -> inferBin env exp1 exp2
-                ELess exp1 exp2 -> case inferBin env exp1 exp2 of
-                                Ok _ -> Ok Bool
-                                _ -> Bad "Bad type"                
-                EGre exp1 exp2 -> case inferBin env exp1 exp2 of
-                                Ok _ -> Ok Bool
-                                _ -> Bad "Bad type"
-                ELeq exp1 exp2 -> case infeBin env exp1 exp2 of
-                                Ok _ -> Ok Bool
-                                _ -> Bad "Bad type"
-                EGeq exp1 exp2 -> case inferBin env exp1 exp2 of
-                                Ok _ -> Ok Bool
-                                _ -> Bad "Bad type"
-                EEqua exp1 exp2 -> case inferExp env exp1 of
-                                Ok Bool -> case inferExp env exp2 of
-                                            Ok Bool -> Ok Bool
-                                            _ -> Bad "Bad type"
-                                Bad _ -> Bad "Bad type"
-                                _ -> case inferExp env exp2 of
-                                    Ok Bool -> bad "Bad type"
-                                    Bad _ -> Bad "Bad type"
-                                    _ -> Ok Bool
-                EIneq exp1 exp2 -> case inferExp env exp1 of
-                                Ok Bool -> case inferExp env exp2 of
-                                            Ok Bool -> Ok Bool
-                                            _ -> Bad "Bad type"
-                                Bad _ -> Bad "Bad type"
-                                _ -> case inferExp env exp2 of
-                                    Ok Bool -> bad "Bad type"
-                                    Bad _ -> Bad "Bad type"
-                                    _ -> Ok Bool
-                EConj exp exp -> case inferExp env exp1 of
-                                    Ok Bool -> case inferExp env exp2 of
-                                                Ok Bool -> Ok Bool
-                                                _ -> Bad "Wrong type"
-                                    Bad _ -> "Not valid type"
-                EDisj exp exp -> case inferExp env exp1 of
-                                    Ok Bool -> case inferExp env exp2 of
-                                                Ok Bool -> Ok Bool
-                                                _ -> Bad "Wrong type"
-                                    Bad _ -> "Not valid type"
-                EAss id exp ->
-                ETyped exp typ ->
+                    EInc id -> case lookupVar env id of
+                                Ok Bool -> Bad "Not valid type"
+                                _ -> lookupVar env id
+                    EDec id ->  case lookupVar env id of
+                                Ok Bool -> Bad "Not valid type"
+                                _ -> lookupVar env id
+                    EInc2 id ->  case lookupVar env id of
+                                Ok Bool -> Bad "Not valid type"
+                                _ -> lookupVar env id
+                    EDec2 id ->  case lookupVar env id of
+                                Ok Bool -> Bad "Not valid type"
+                                _ -> lookupVar env id
+                    EMul exp1 exp2 -> inferBin env exp1 exp2
+                    EDiv exp1 exp2 -> inferBin env exp1 exp2
+                    EAdd exp1 exp2 -> inferBin env exp1 exp2
+                    ESub exp1 exp2 -> inferBin env exp1 exp2
+                    ELess exp1 exp2 -> case inferBin env exp1 exp2 of
+                                        Ok _ -> Ok Bool
+                                        _ -> Bad "Bad type"                
+                    EGre exp1 exp2 -> case inferBin env exp1 exp2 of
+                                        Ok _ -> Ok Bool
+                                        _ -> Bad "Bad type"
+                    ELeq exp1 exp2 -> case infeBin env exp1 exp2 of
+                                        Ok _ -> Ok Bool
+                                        _ -> Bad "Bad type"
+                    EGeq exp1 exp2 -> case inferBin env exp1 exp2 of
+                                        Ok _ -> Ok Bool
+                                        _ -> Bad "Bad type"
+                    EEqua exp1 exp2 -> case inferExp env exp1 of
+                                        Ok Bool -> case inferExp env exp2 of
+                                                    Ok Bool -> Ok Bool
+                                                    _ -> Bad "Bad type"
+                                        Bad _ -> Bad "Bad type"
+                                        _ -> case inferExp env exp2 of
+                                                    Ok Bool -> bad "Bad type"
+                                                    Bad _ -> Bad "Bad type"
+                                                    _ -> Ok Bool
+                    EIneq exp1 exp2 -> case inferExp env exp1 of
+                                        Ok Bool -> case inferExp env exp2 of
+                                                    Ok Bool -> Ok Bool
+                                                    _ -> Bad "Bad type"
+                                        Bad _ -> Bad "Bad type"
+                                        _ -> case inferExp env exp2 of
+                                                    Ok Bool -> bad "Bad type"
+                                                    Bad _ -> Bad "Bad type"
+                                                    _ -> Ok Bool
+                    EConj exp exp -> case inferExp env exp1 of
+                                        Ok Bool -> case inferExp env exp2 of
+                                                    Ok Bool -> Ok Bool
+                                                    _ -> Bad "Wrong type"
+                                        Bad _ -> "Not valid type"
+                    EDisj exp exp -> case inferExp env exp1 of
+                                        Ok Bool -> case inferExp env exp2 of
+                                                    Ok Bool -> Ok Bool
+                                                    _ -> Bad "Wrong type"
+                                        Bad _ -> "Not valid type"
+                    EAss id exp -> case lookupVar env id of
+                                    Bad s -> Bad s
+                                    Ok typ -> case checkExp env typ exp of
+                                                Bad s -> Bad s
+                                                Ok _ -> Ok typ 
+                    ETyped exp typ -> case checkExp env typ exp of
+                                        Ok _ -> Ok typ
+                                        Bad s -> Bad s
 
 isBad :: Err a -> Bool
 isBad Bad _ = True
@@ -136,6 +141,18 @@ checkExp env typ exp = do
                "but found " ++ printTree typ2
 
 checkStms :: Env -> Stms -> Err ()
+checkStms env stms = case stms of 
+                        SExp exp -> case inferExp env exp of
+                                        Ok _ -> return ()
+                                        _ -> Bad "Statement with faulty types"
+                        SDecls typ ids -> map updateVar env ids typ
+                        SInit typ id exp -> case checkExp env typ exp of
+                                            Ok _ -> updateVar env id typ
+                                            Bad s -> Bad s
+                        SRet exp -> 
+                        SWhile exp stm -> 
+                        SIf exp stm stm -> 
+                        SBlock stmxs -> 
 
 checkDef :: Env -> Def -> Err ()
 
