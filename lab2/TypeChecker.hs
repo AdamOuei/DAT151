6c1d8c1c3ae644511a0f1c1a1ad27df750a845ce
Omar Oueidat <oueidat@student.chalmers.se>
2019-11-25 10:48:17 +0100

    For my man Tubs

--- lab2/TypeChecker.hs
+++ lab2/TypeChecker.hs
@@ -31,25 +31,99 @@ inferExp env exp =
                                 Bad s -> Bad s
                                 Ok (argsTypes, typ) -> do
                                                 let argsCheck = zipWith (\x y -> checkExp env x y) argTypes argExps
-                                                
-                EDec id -> 
-                EInc2 id ->
-                EDec2 id ->
-                EMul exp exp ->
-                EDiv exp exp ->
-                EAdd exp exp ->
-                ESub exp exp ->
-                ELess exp exp ->
-                EGre exp exp ->
-                ELeq exp exp ->
-                EGeq exp exp ->
-                EEqua exp exp ->
-                EIneq exp exp ->
-                EConj exp exp ->
-                EDisj exp exp ->
+                                                case any isBad argsCheck of
+                                                    True -> Bad "Arguments not ok"
+                                                    False -> Ok typ
+                -- Maybe change to TBool and add void
+                EInc id -> case lookupVar env id of
+                            Ok Bool -> Bad "Not valid type"
+                            _ -> lookupVar env id
+                EDec id ->  case lookupVar env id of
+                            Ok Bool -> Bad "Not valid type"
+                            _ -> lookupVar env id
+                EInc2 id ->  case lookupVar env id of
+                            Ok Bool -> Bad "Not valid type"
+                            _ -> lookupVar env id
+                EDec2 id ->  case lookupVar env id of
+                            Ok Bool -> Bad "Not valid type"
+                            _ -> lookupVar env id
+                EMul exp1 exp2 -> inferBin env exp1 exp2
+                EDiv exp1 exp2 -> inferBin env exp1 exp2
+                EAdd exp1 exp2 -> inferBin env exp1 exp2
+                ESub exp1 exp2 -> inferBin env exp1 exp2
+                ELess exp1 exp2 -> case inferBin env exp1 exp2 of
+                                Ok _ -> Ok Bool
+                                _ -> Bad "Bad type"                
+                EGre exp1 exp2 -> case inferBin env exp1 exp2 of
+                                Ok _ -> Ok Bool
+                                _ -> Bad "Bad type"
+                ELeq exp1 exp2 -> case infeBin env exp1 exp2 of
+                                Ok _ -> Ok Bool
+                                _ -> Bad "Bad type"
+                EGeq exp1 exp2 -> case inferBin env exp1 exp2 of
+                                Ok _ -> Ok Bool
+                                _ -> Bad "Bad type"
+                EEqua exp1 exp2 -> case inferExp env exp1 of
+                                Ok Bool -> case inferExp env exp2 of
+                                            Ok Bool -> Ok Bool
+                                            _ -> Bad "Bad type"
+                                Bad _ -> Bad "Bad type"
+                                _ -> case inferExp env exp2 of
+                                    Ok Bool -> bad "Bad type"
+                                    Bad _ -> Bad "Bad type"
+                                    _ -> Ok Bool
+                EIneq exp1 exp2 -> case inferExp env exp1 of
+                                Ok Bool -> case inferExp env exp2 of
+                                            Ok Bool -> Ok Bool
+                                            _ -> Bad "Bad type"
+                                Bad _ -> Bad "Bad type"
+                                _ -> case inferExp env exp2 of
+                                    Ok Bool -> bad "Bad type"
+                                    Bad _ -> Bad "Bad type"
+                                    _ -> Ok Bool
+                EConj exp exp -> case inferExp env exp1 of
+                                    Ok Bool -> case inferExp env exp2 of
+                                                Ok Bool -> Ok Bool
+                                                _ -> Bad "Wrong type"
+                                    Bad _ -> "Not valid type"
+                EDisj exp exp -> case inferExp env exp1 of
+                                    Ok Bool -> case inferExp env exp2 of
+                                                Ok Bool -> Ok Bool
+                                                _ -> Bad "Wrong type"
+                                    Bad _ -> "Not valid type"
                 EAss id exp ->
                 ETyped exp typ ->
 
+isBad :: Err a -> Bool
+isBad Bad _ = True
+isBad _ = False
+
+inferBin :: Env -> Exp -> Exp -> Err Type
+inferBin env exp1 exp2 = 
+                        case inferExp env exp1 of
+                        Ok Integer -> case inferExp env exp2 of
+                            Ok Integer -> Ok Integer
+                            Ok Double -> Ok Double
+                            _ -> Bad "Bad type"
+                        Ok Double -> case inferExp env exp2 of
+                            Ok Bool -> Bad "Bad type"
+                            Bad x -> Bad x
+                            _ -> Ok Double
+                        _ -> Bad "Bad type"
+
+
+-- do
+--                     type1 = inferExp env exp1
+--                     type2 = inferExp env exp2
+--                     if type1 `elem` types
+--                         then
+--                             if type2 `elem` types
+--                                 then
+--                             case checkExp env type1 exp2 of
+--                                 Bad s -> Bad s
+--                                 _ -> 
+--                         else 
+--                             Bad "Not a binary type"
 
 checkExp :: Env -> Type -> Exp -> Err ()
 checkExp env typ exp = do
@@ -57,7 +131,7 @@ checkExp env typ exp = do
     if (typ2 == typ) then
         return ()
         else
-        fail $ "type of " ++ printTree exp ++
+        Bad $ "type of " ++ printTree exp ++
                "expected " ++ printTree typ ++
                "but found " ++ printTree typ2
 
