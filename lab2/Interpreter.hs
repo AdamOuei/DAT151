36ad70e01af623003dfa3918c054a0ce9849f6a4
Omar Oueidat <oueidat@student.chalmers.se>
2019-11-29 13:40:17 +0100

    More work on lab2

--- lab2/Interpreter.hs
+++ lab2/Interpreter.hs
@@ -20,28 +20,121 @@ data Val = VInt Integer | VDouble Double | VBool Bool | VVoid
 interpret :: Program -> IO ()
 interpret p = putStrLn "no interpreter yet"
 
-eval ::  Env -> Exp -> IO (Val, Env)
+evalExp ::  Env -> Exp -> IO (Val, Env)
+evalExp env@(sig,top:context) exp =  case exp of
+                        EInt int -> return (VInt int, env)
+                        EDouble doub -> return (VDouble doub, env)
+                        ETrue -> return (VBool True, env)
+                        EFalse -> return (VBool True, env)
+                        EId id -> do 
+                            val <- lookupVar env id 
+                            return (val,env)
+                        ECall id argExps ->
+                                        case lookupFun env id of
+                                            Bad s -> Bad s
+                                            Ok (argsTypes, typ) -> do
+                                                                    mapM_ (uncurry $ checkExp env) $ zip  argsTypes argExps
+                                                                    return typ
+                        -- Maybe change to TBool and add void
+                        EInc id -> case lookupVar env id of
+                                    Ok TBool -> Bad "Not valid type"
+                                    _ -> lookupVar env id
+                        EDec id ->  case lookupVar env id of
+                                    Ok TBool -> Bad "Not valid type"
+                                    _ -> lookupVar env id
+                        EInc2 id ->  case lookupVar env id of
+                                    Ok TBool -> Bad "Not valid type"
+                                    _ -> lookupVar env id
+                        EDec2 id ->  case lookupVar env id of
+                                    Ok TBool -> Bad "Not valid type"
+                                    _ -> lookupVar env id
+                        EMul exp1 exp2 -> inferBin env exp1 exp2
+                        EDiv exp1 exp2 -> inferBin env exp1 exp2
+                        EAdd exp1 exp2 -> inferBin env exp1 exp2
+                        ESub exp1 exp2 -> inferBin env exp1 exp2
+                        ELess exp1 exp2 -> case inferBin env exp1 exp2 of
+                                            Ok _ -> Ok TBool
+                                            _ -> Bad "Bad type"                
+                        EGre exp1 exp2 -> case inferBin env exp1 exp2 of
+                                            Ok _ -> Ok TBool
+                                            _ -> Bad "Bad type"
+                        ELeq exp1 exp2 -> case inferBin env exp1 exp2 of
+                                            Ok _ -> Ok TBool
+                                            _ -> Bad "Bad type"
+                        EGeq exp1 exp2 -> case inferBin env exp1 exp2 of
+                                            Ok _ -> Ok TBool
+                                            _ -> Bad "Bad type"
+                        EEqua exp1 exp2 -> case inferExp env exp1 of
+                                            Ok TBool -> case inferExp env exp2 of
+                                                        Ok TBool -> Ok TBool
+                                                        _ -> Bad "Bad type"
+                                            Bad _ -> Bad "Bad type"
+                                            _ -> case inferExp env exp2 of
+                                                        Ok TBool -> Bad "Bad type"
+                                                        Bad _ -> Bad "Bad type"
+                                                        _ -> Ok TBool
+                        EIneq exp1 exp2 -> case inferExp env exp1 of
+                                            Ok TBool -> case inferExp env exp2 of
+                                                        Ok TBool -> Ok TBool
+                                                        _ -> Bad "Bad type"
+                                            Bad _ -> Bad "Bad type"
+                                            _ -> case inferExp env exp2 of
+                                                        Ok TBool -> Bad "Bad type"
+                                                        Bad _ -> Bad "Bad type"
+                                                        _ -> Ok TBool
+                        EConj exp1 exp2 -> case inferExp env exp1 of
+                                            Ok TBool -> case inferExp env exp2 of
+                                                        Ok TBool -> Ok TBool
+                                                        _ -> Bad "Wrong type"
+                                            Bad _ -> Bad "Not valid type"
+                        EDisj exp1 exp2 -> case inferExp env exp1 of
+                                            Ok TBool -> case inferExp env exp2 of
+                                                        Ok TBool -> Ok TBool
+                                                        _ -> Bad "Wrong type"
+                                            Bad _ -> Bad "Not valid type"
+                        EAss id exp -> case lookupVar env id of
+                                        Bad s -> Bad s
+                                        Ok typ -> case checkExp env typ exp of
+                                                    Bad s -> Bad s
+                                                    Ok _ -> Ok typ 
+                        ETyped exp typ -> case checkExp env typ exp of
+                                            Ok _ -> Ok typ
+                                            Bad s -> Bad s
 
 execProg :: Env -> IO ()
 
 execStm :: Env -> Stm -> IO (Maybe Val, Env)
+execStm env@(sig,top:context) stm = case stm of 
+        SExp exp -> do
+        SDecls typ (id:ids) -> 
+        SDecls typ [] -> 
+        SInit typ id exp -> 
+        SRet exp -> 
+        SWhile exp stm -> 
+        SIf exp stm1 stm2 -> 
+        SBlock stmxs -> 
+                
 
 lookupVar :: Env -> Id -> IO Val
+lookupVar (_, []) _ = fail "No variable found"
+lookupVar (sig,first:context) id = case Map.lookup id first of
+                                    Just (Just val) -> return val
+                                    Just Nothing -> fail "Variable not found"
+                                    Nothing -> fail "Variable not found"
 
 lookupFun :: Env -> Id -> IO Func
+lookupFun (sig, _) id = case Map.lookup id sig of
+                            Nothing -> fail "Function not found"
+                            Just fun -> return fun
 
 exitBlock :: Env -> Env
+exitBlock (sig, firstBlock:context)= (sig, context) 
 
 newBlock :: Env -> Env
+newBlock (sig,context) = (sig, Map.empty:context)
 
 emptyEnv :: Env
-
-
-
-
-
-
-
+emptyEnv  = (Map.empty,[Map.empty])
 
 addValue :: Val -> Val -> Val
 addValue (VInt a) (VInt b) = VInt $ a + b 
